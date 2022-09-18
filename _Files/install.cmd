8190f77e936559ca7858985fa3c72886e00b34b1
Marvin700 <98750428+Marvin700@users.noreply.github.com>
2022-09-18 22:01:41 +0200

    Update install.cmd

--- _Files/install.cmd
+++ _Files/install.cmd
@@ -91,5 +91,3 @@ echo ---------------------------
 echo Schritt 7 - Sophia Script
 echo ---------------------------
 Powershell.exe -executionpolicy remotesigned -File "C:\Windows_Optimisation_Pack\_Files\Sophia Script\Sophia.ps1"
-
-exit
