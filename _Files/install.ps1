ee7e5ce12bb253df941b51c89a690f86edeb1248
Marvin <98750428+Marvin700@users.noreply.github.com>
2022-09-27 19:29:03 +0200

    Update install.ps1

--- _Files/install.ps1
+++ _Files/install.ps1
@@ -52,7 +52,7 @@ Expand-Archive 'C:\Windows_Optimisation_Pack\_Files\Autoruns.zip' 'C:\Windows_Op
 Remove-Item -Path C:\Windows_Optimisation_Pack\_Files\Autoruns.zip -Force -Recurse
 Move-Item -Path "C:\Windows_Optimisation_Pack\_Files\Autoruns\Autoruns64.exe" -Destination "C:\Windows_Optimisation_Pack\_Files\Autoruns.exe" -Force
 Remove-Item "C:\Windows_Optimisation_Pack\_Files\Autoruns\" -force -Recurse
-winget install --id=RARLab.WinRAR -e
+# winget install --id=RARLab.WinRAR -e
 Clear-Host
 
 "---------------------------"
