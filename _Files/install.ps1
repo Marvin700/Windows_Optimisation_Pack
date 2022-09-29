268ac4349eebff3ab693efa3069e414eaf960d6f
Marvin <98750428+Marvin700@users.noreply.github.com>
2022-09-29 15:17:30 +0200

    Update install.ps1

--- _Files/install.ps1
+++ _Files/install.ps1
@@ -53,10 +53,9 @@ Clear-Host
 "--------------------------------------------------------"
 #Windows Version bestimmen
 $WindowsVersion = (Get-WmiObject -class Win32_OperatingSystem).Caption
+
 Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.3.2091/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile C:\Windows_Optimisation_Pack\_Files\winget.msixbundle
 invoke-expression 'cmd /c start powershell -Command { add-AppxPackage -Path C:\Windows_Optimisation_Pack\_Files\WinGet.msixbundle;winget source update;Remove-Item -Path C:\Windows_Optimisation_Pack\_Files\winget.msixbundle -Force -Recurse}'
-Remove-Item -Path C:\Windows_Optimisation_Pack\_Files\winget.msixbundle -Force -Recurse
-
 
 Invoke-WebRequest 'https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe' -OutFile C:\Windows_Optimisation_Pack\_Files\OOSU10.exe
 
