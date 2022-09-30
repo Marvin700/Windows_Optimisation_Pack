e102806160952c8d415466dd5a23efc0e5314f19
Marvin <98750428+Marvin700@users.noreply.github.com>
2022-09-30 23:46:06 +0200

    Update install.ps1

--- _Files/install.ps1
+++ _Files/install.ps1
@@ -188,8 +188,6 @@ winget upgrade --all
 " -----------------------------------------------"
 winget install --id=RARLab.WinRAR --exact --accept-source-agreements
 ""
-winget install --id=Notepad++.Notepad++ --accept-source-agreements
-""
 winget install --id=REALiX.HWiNFO --exact --accept-source-agreements
 Stop-Process -Name HWiNFO64
 ""
