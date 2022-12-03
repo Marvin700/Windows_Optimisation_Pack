3c04fdb1d5c5c2a201cb20fc0fb1ccbb5bb93682
Marvin <98750428+Marvin700@users.noreply.github.com>
2022-12-03 10:40:49 +0100

    Delete signpack.ps1

--- signpack.ps1
+++
@@ -1,3 +0,0 @@
-cd Cert:\CurrentUser\My\
-$Cert = Get-ChildItem "7B29DE403EDBD036E73DA32BA83437090EC2CA16"
-Set-AuthenticodeSignature -Certificate:$cert -FilePath:"C:\Windows_Optimisation_Pack\_Files\Windows-Cleaner.ps1"
