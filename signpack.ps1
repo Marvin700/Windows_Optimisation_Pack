69fb2386935a19d426885a9cd9c1ada98f3fd65c
Marvin Lange <98750428+Marvin700@users.noreply.github.com>
2023-03-26 18:05:08 +0200

    Add files via upload

---
+++ signpack.ps1
@@ -0,0 +1,4 @@
+Set-Location Cert:\CurrentUser\My\
+$Cert = Get-ChildItem "7B29DE403EDBD036E73DA32BA83437090EC2CA16"
+Set-AuthenticodeSignature -Certificate:$cert -FilePath:"C:\Windows_Optimisation_Pack\_Files\*"
+Set-AuthenticodeSignature -Certificate:$cert -FilePath:"C:\Windows_Optimisation_Pack\_Files\config\*"
