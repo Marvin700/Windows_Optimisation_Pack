4bc504b97dfc19a7b860aa3890b2882ec3983d0f
Marvin Lange <98750428+Marvin700@users.noreply.github.com>
2025-09-21 11:11:45 +0200

    Add files via upload

--- config/Windows-Health.ps1
+++ config/Windows-Health.ps1
@@ -1,3 +1,8 @@
+# Windows_Optimisation_Pack @Marvin700
+# windows-optimisation.de
+
+$Branch = "Beta"
+
 Dism /Online /Cleanup-Image /ScanHealth
 Dism /Online /Cleanup-Image /RestoreHealth
 sfc /SCANNOW
