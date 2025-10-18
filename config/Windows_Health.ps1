22e40227a353f1fdd60311d323a31ca944f8a541
Marvin Lange <98750428+Marvin700@users.noreply.github.com>
2025-10-18 15:50:30 +0200

    Delete config/Windows_Health.ps1

--- config/Windows_Health.ps1
+++
@@ -1,8 +0,0 @@
-# Windows_Optimisation_Pack @Marvin700
-# windows-optimisation.de
-
-$Branch = "Beta"
-
-Dism /Online /Cleanup-Image /ScanHealth
-Dism /Online /Cleanup-Image /RestoreHealth
-sfc /SCANNOW
